8f5f0e6072c7504a31a28e500d936bfc3698519d
jamesvthompson <109220448+jamesvthompson@users.noreply.github.com>
2023-02-08 19:57:48 -0500

    Update worker-install-k8s.sh

--- scripts/ubuntu20/worker-install-k8s.sh
+++ scripts/ubuntu20/worker-install-k8s.sh
@@ -81,7 +81,7 @@ sudo systemctl enable kubelet
 
 # Join the cluster using the kubeadm join command
 read -p "Enter the kubeadm join command from the master node: " join_command
-$join_command
+sudo $join_command
 
 # Check whether all the default pods are running
 watch kubectl get pods -A
